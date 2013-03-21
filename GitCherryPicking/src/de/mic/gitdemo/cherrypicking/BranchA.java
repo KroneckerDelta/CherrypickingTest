59ca31195aae005d46e8c911753b0e8c3fa4310d
Thomas Michael <thomas.michael@anmi24.de>
2013-03-21 19:39:48 +0100

    init and create class for branchA

---
+++ GitCherryPicking/src/de/mic/gitdemo/cherrypicking/BranchA.java
@@ -0,0 +1,12 @@
+package de.mic.gitdemo.cherrypicking;
+/**
+ * This Class starts in BranchA und shold later merge with another branch
+ * @author thomicha
+ *
+ */
+public class BranchA {
+
+	private void doNothing() {
+		// this place is for your commercial
+	}
+}
