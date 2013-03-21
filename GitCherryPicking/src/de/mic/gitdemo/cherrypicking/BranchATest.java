1b929cda06566fbca60f8b6d3cd62149ec48a25d
Thomas Michael <thomas.michael@anmi24.de>
2013-03-21 19:55:37 +0100

    added test for doing something

---
+++ GitCherryPicking/src/de/mic/gitdemo/cherrypicking/BranchATest.java
@@ -0,0 +1,16 @@
+package de.mic.gitdemo.cherrypicking;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BranchATest {
+
+	@Test
+	public void shouldDoSomething() {
+		
+		// this test is very slow
+		assertTrue(true);
+		//really!
+	}
+}
